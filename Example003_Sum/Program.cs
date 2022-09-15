37b69237889df346e40ec841f576176f5a80b220
FreeRadical1320 <vlad.pronnikov@gmail.com>
2022-09-15 15:56:26 +0300

    Random numbers

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 7;
-int numberB = 13;
+﻿int numberA = new Random().Next(1, 10); // Random number: 1, 2, 3, 4 ... 9 
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
