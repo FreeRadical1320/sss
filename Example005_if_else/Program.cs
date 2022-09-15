e87b56663e2acf6b91cd9ae0e016717bdbb168c7
FreeRadical1320 <vlad.pronnikov@gmail.com>
2022-09-15 17:04:51 +0300

    Example 5 if_else condition

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Please, input user name ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Huray! Here is Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
