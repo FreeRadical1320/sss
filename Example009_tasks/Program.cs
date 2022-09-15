07099d1626460743c8b6d11161e03700c0a03018
FreeRadical1320 <vlad.pronnikov@gmail.com>
2022-09-15 22:20:48 +0300

    Practise tasks

---
+++ Example009_tasks/Program.cs
@@ -0,0 +1,33 @@
+﻿// // User input
+Console.Clear();
+Console.Write("Please, input N = ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int max = 0;
+
+if(N < 0)
+{
+    count = N;
+    max = -N;
+}
+else
+{
+    count = -N;
+    max = N;
+}
+
+while(count <= N)
+{
+    Console.WriteLine(count);
+    count += 1;
+} 
+
+
+
+// User input
+Console.Clear();
+Console.Write("Please, input N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = N%10;
+Console.WriteLine($"Last digit = {result}");
