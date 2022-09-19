048f38255886732b5dc40b600803b2000b8434d2
FreeRadical1320 <vlad.pronnikov@gmail.com>
2022-09-19 17:09:36 +0300

    methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,33 +1,47 @@
-﻿
-// int Max(int arg1, int arg2, int arg3)
-// {
-//     int result = arg1;
-//     if (arg2 > result) result = arg2;
-//     if (arg3 > result) result = arg3;
-
-//     return result;
-// }
-// //              0   1   2   3   4   5   6   7   8
-// int[] array = {15, 21, 39, 12, 23, 33, 13, 23, 313};
-
-// int max = Max(
-//     Max(array[0], array[1], array[2]), 
-//     Max(array[3], array[4], array[5]), 
-//     Max(array[6], array[7], array[8]));
-
-// Console.WriteLine(max);
-
-int[] array = {1, 12, 31, 4, 18, 16, 17, 18};
-int n = array.Length;
-int find = 18;
-int index = 0;
-
-while (index < n)
+﻿void FillArray(int[] collection)
 {
-    if (array[index] == find)
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
     {
-        Console.WriteLine(index);
-        break;
+        collection[index] = new Random().Next(1, 10);
+        index++;
     }
-    index++;
 }
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
