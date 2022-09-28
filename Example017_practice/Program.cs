fa04fcc02d10b4143fe69589f7705c73f06bf4bf
FreeRadical1320 <vlad.pronnikov@gmail.com>
2022-09-28 13:09:57 +0300

    practice 3

---
+++ Example017_practice/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+// Способы инициализации массива
+
+// int[] numbers = new int[5]; 
+// int[] numbers = new int[5] {1, 2, 3, 4, 5}; 
+// int[] numbers = new int[] {1, 2, 3, 4, 5}; 
+// int[] numbers = new [] {1, 2, 3, 4, 5}; 
+int[] numbers = {1, 2, 3, 4, 5}; 
+
+string[] name = {"Ann", "Bob", "John", "Kris", "Ama"};
+
+
+// Console.WriteLine($"{numbers[3]} !!!");
+// numbers[3] += 10;
+
+// Console.WriteLine($"{numbers[3]} !!!");
+
+// for(int i = 0; i < 5; i++) 
+//     Console.WriteLine($"{i} element is {name[i]} !!!");
+
+Console.WriteLine($"{numbers[3]} !!!");
+
+Console.WriteLine($"{Math.Pow(numbers[1], 20)} !!!");
