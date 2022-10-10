4d4f287c741c7c3eb3ed3cee4406e1310b75c88a
FreeRadical1320 <vlad.pronnikov@gmail.com>
2022-10-10 15:23:31 +0300

    practice_07

---
+++ Example022_practice/Program.cs
@@ -0,0 +1,156 @@
+﻿// Задача 39:** Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+Console.Clear();
+
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write($"{arr[i]}\t");
+    }
+    Console.WriteLine();
+}
+
+int[] FillArray(int lng, int lowBorder, int highBorder)
+{
+    int[] arr = new int[lng];
+    for(int i = 0; i < lng; i++)
+    {
+        arr[i] = new Random().Next(lowBorder, highBorder + 1);
+    }
+    return arr;
+}
+// int[] arr = FillArray(15, 100, 999);
+
+// PrintArray(arr);
+
+int[] ReverseArray(int[] arr)
+{
+    int tmp;
+    int j = arr.Length - 1;
+    for(int i = 0; i < j; i++)
+    {
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+        j--;
+    }
+    return arr;
+}
+
+// PrintArray(ReverseArray(arr));
+
+// Задача 40:** Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+// **Теорема о неравенстве треугольника:** каждая сторона треугольника меньше суммы двух других сторон.
+
+// Console.Write("Please, input a = ");
+
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Please, input b = ");
+
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Please, input c = ");
+
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// string isExist = string.Empty;
+
+// if((a + b > c) && (a + c > b) && (c + b > a)) isExist = "exist";
+// else isExist = "not exist";
+
+// Console.WriteLine($"Triangle with the sizes {a}, {b} and {c} {isExist}.");
+
+
+// Задача 42:** Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+// Console.Write("Please, input N = ");
+
+// double N = Convert.ToInt32(Console.ReadLine());
+
+// int[] binary = new int[16];
+// double factor = 0;
+
+// for(int i = 15; i >= 0; i--)
+// {
+//     factor = Math.Pow(2, i);
+//     if(N >= factor)
+//     {
+//         binary[i] = 1;
+//         N -= factor;
+//     } 
+// }
+
+// PrintArray(binary);  
+// PrintArray(ReverseArray(binary));  
+
+Console.Write("Please, input N = ");
+
+int dec = Convert.ToInt32(Console.ReadLine());
+
+string binary = string.Empty;
+
+while(dec != 0)
+{
+    binary = Convert.ToString(dec % 2) + binary;
+    dec = dec / 2;
+}
+
+Console.WriteLine(binary);   
+
+
+
+// Задача 44:** Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+// Console.Write("Please, input N = ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int[] arr;
+// if(N > 1)
+// {
+//     arr = new int[N];
+//     arr[0] = 0;
+//     arr[1] = 1;
+//     if(N > 2)
+//     {
+//         for(int i = 2; i < N; i++)
+//         {
+//             arr[i] = arr[i - 1] + arr[i - 2];
+//         }
+//     }
+// } 
+// else
+// {
+//     arr = new int[1] {0};
+// }
+
+// PrintArray(arr);
+
+// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+// int[] CopyArray(int[] arr)
+// {
+//     int lng = arr.Length;
+//     int[] new_arr = new int[lng];
+//     for(int i = 0; i < lng; i++)
+//     {
+//         new_arr[i] = arr[i];
+//     }
+//     return new_arr;
+// }
+// int[] arr = FillArray(7, -100, 100);
+
+// PrintArray(arr);
+
+// PrintArray(CopyArray(arr));
---
+++ Example023_practice_07/Program.cs
@@ -0,0 +1,104 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+Console.Clear();
+
+void PrintArray(double[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] FillArray(int row, int col, int lowBorder, int highBorder)
+{
+    double[,] arr = new double[row, col];
+    for(int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr[i, j] = new Random().Next(lowBorder, highBorder + 1);
+        }
+    }
+    return arr;
+}
+double[,] arr = FillArray(5, 5, 100, 1000);
+
+PrintArray(arr);
+
+// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// int[,] FillArrayWithIndexSum(int row, int col)
+// {
+//     int[,] arr = new int[row, col];
+//     for(int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j < col; j++)
+//         {
+//             arr[i, j] = i + j;
+//         }
+//     }
+//     return arr;
+// }
+// int[,] arr = FillArrayWithIndexSum(3, 4);
+// PrintArray(arr);
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Console.WriteLine();
+
+// double[,] ReplaceEvenNumbers(double[,] arr)
+// {
+//     for(int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             if( i != 0 
+//                 && i % 2 == 0
+//                 && j != 0
+//                 && j% 2 == 0)
+//             arr[i, j] = Math.Pow(arr[i, j], 2);
+//         }
+//     }
+//     return arr;
+// }
+// double[,] new_arr = ReplaceEvenNumbers(arr);
+// PrintArray(new_arr);
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+double FindDiagonalSum(double[,] arr)
+{
+    double sum = 0;
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(i == j)
+            sum += arr[i, j];
+        }
+    }
+    return sum;
+}
+Console.WriteLine($"The diagonal sum of the matrix is equal: {FindDiagonalSum(arr)}.")
